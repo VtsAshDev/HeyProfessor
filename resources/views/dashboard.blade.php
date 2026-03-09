2367823412122f108d69edb2e1844c7da0656ca7
VtsAshDev <vitorgguimaraes56@gmail.com>
2026-03-09 18:46:47 -0300

    HEY-9: refactor to use blade components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,17 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    <x-header>
+        <x-slot name="header">
             {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+        </x-slot>
+    </x-header>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action = "{{route('question.store')}}" method="post">
-                        @csrf
-                        <div class="mb-4">
-                            <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your question</label>
-                            <textarea name="question" id="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your question to your professor here...">{{ old('question') }}</textarea>
-                        </div>
-                        @error('question')
-                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                            <span class="font-medium">{{$message}}</span>
-                        </div>
-                        @enderror
-                        <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
-                        <button type="reset" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancel</button>
-                   </form>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <x-container>
+        <x-form post :action="route('question.store')">
+            <x-text-area label="Question" name="question"/>
+
+            <x-btn.primary>Save</x-btn.primary>
+            <x-btn.reset>Cancel</x-btn.reset>
+        </x-form>
+    </x-container>
 </x-app-layout>
